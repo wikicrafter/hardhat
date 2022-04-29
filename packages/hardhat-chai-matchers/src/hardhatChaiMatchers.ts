2d6fa41766ca2dd723f23453852d41b7433e329f
Franco Victorio <victorio.franco@gmail.com>
2022-04-29 20:24:26 -0300

    Refactor and simplify reverted matchers

--- packages/hardhat-chai-matchers/src/hardhatChaiMatchers.ts
+++ packages/hardhat-chai-matchers/src/hardhatChaiMatchers.ts
@@ -1,7 +1,11 @@
 import "./types";
 import { supportBigNumber } from "./bigNumber";
 import { supportEmit } from "./emit";
-import { supportReverted } from "./reverted";
+import { supportReverted } from "./reverted/reverted";
+import { supportRevertedWith } from "./reverted/revertedWith";
+import { supportRevertedWithCustomError } from "./reverted/revertedWithCustomError";
+import { supportRevertedWithPanic } from "./reverted/revertedWithPanic";
+import { supportRevertedWithoutReasonString } from "./reverted/revertedWithoutReasonString";
 
 export function hardhatChaiMatchers(
   chai: Chai.ChaiStatic,
@@ -9,5 +13,9 @@ export function hardhatChaiMatchers(
 ) {
   supportBigNumber(chai.Assertion, utils);
   supportEmit(chai.Assertion);
-  supportReverted(chai.Assertion, utils);
+  supportReverted(chai.Assertion);
+  supportRevertedWith(chai.Assertion);
+  supportRevertedWithCustomError(chai.Assertion, utils);
+  supportRevertedWithPanic(chai.Assertion);
+  supportRevertedWithoutReasonString(chai.Assertion);
 }
